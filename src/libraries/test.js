449f01e503096327e79c690a5d01348d2aaf43cf
miltonbolonha <miltonbolonha@gmail.com>
2021-10-25 18:09:03 -0300

    integração jogo template

---
+++ src/libraries/test.js
@@ -0,0 +1,5 @@
+const Test = () => {
+  console.log("nnneeemmm");
+};
+
+export default Test;
--- src/modules/layout/components/Main.js
+++ src/modules/layout/components/Main.js
@@ -4,7 +4,80 @@ import { StaticImage } from "gatsby-plugin-image";
 const Main = () => (
   <>
     <div className='main-content'>
-      <main>ooiii</main>
+      <div class='container'>
+        <header>
+          <h1>Jogo Da Memória</h1>
+        </header>
+
+        <section class='score-panel'>
+          <ul class='stars'>
+            <li>
+              <i class='fa fa-star'></i>
+            </li>
+            <li>
+              <i class='fa fa-star'></i>
+            </li>
+            <li>
+              <i class='fa fa-star'></i>
+            </li>
+          </ul>
+          <span class='moves'>0</span> Moves |<span id='timer'>0</span>s
+          <div class='restart'>
+            <i class='fa fa-repeat'></i>
+          </div>
+        </section>
+
+        <ul class='deck'>
+          <li class='card'>
+            <i class='fa fa-diamond'></i>
+          </li>
+          <li class='card'>
+            <i class='fa fa-paper-plane-o'></i>
+          </li>
+          <li class='card'>
+            <i class='fa fa-anchor'></i>
+          </li>
+          <li class='card'>
+            <i class='fa fa-bolt'></i>
+          </li>
+          <li class='card'>
+            <i class='fa fa-cube'></i>
+          </li>
+          <li class='card'>
+            <i class='fa fa-anchor'></i>
+          </li>
+          <li class='card'>
+            <i class='fa fa-leaf'></i>
+          </li>
+          <li class='card'>
+            <i class='fa fa-bicycle'></i>
+          </li>
+          <li class='card'>
+            <i class='fa fa-diamond'></i>
+          </li>
+          <li class='card'>
+            <i class='fa fa-bomb'></i>
+          </li>
+          <li class='card'>
+            <i class='fa fa-leaf'></i>
+          </li>
+          <li class='card'>
+            <i class='fa fa-bomb'></i>
+          </li>
+          <li class='card'>
+            <i class='fa fa-bolt'></i>
+          </li>
+          <li class='card'>
+            <i class='fa fa-bicycle'></i>
+          </li>
+          <li class='card'>
+            <i class='fa fa-paper-plane-o'></i>
+          </li>
+          <li class='card'>
+            <i class='fa fa-cube'></i>
+          </li>
+        </ul>
+      </div>
     </div>
   </>
 );
