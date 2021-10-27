b8c24689c18528c9a532668d5df80cff1a491c7c
miltonbolonha <miltonbolonha@gmail.com>
2021-10-27 16:18:11 -0300

    Containerização da lógica

--- src/modules/layout/containers/index.js
+++ src/modules/layout/containers/index.js
@@ -1,56 +1,58 @@
-import { React } from "../dependencies";
-import BodyContainer from "./BodyContainer";
-import HeaderContainer from "./HeaderContainer";
-import FooterContainer from "./FooterContainer";
-import RowContainer from "./RowContainer";
-import FormContainer from "./FormContainer";
-import PlayerContainer from "./PlayerContainer";
-import FullScreenContainer from "./FullScreenContainer";
-import ComponentContainer from "./ComponentContainer";
-import MainContainer from "./MainContainer";
-
+import { React } from '../dependencies'
+import BodyContainer from './BodyContainer'
+import HeaderContainer from './HeaderContainer'
+import FooterContainer from './FooterContainer'
+import RowContainer from './RowContainer'
+import FormContainer from './FormContainer'
+import PlayerContainer from './PlayerContainer'
+import FullScreenContainer from './FullScreenContainer'
+import ComponentContainer from './ComponentContainer'
+import MainContainer from './MainContainer'
+import MemoryGameContainer from './MemoryGameContainer'
 const LayoutResolver = ({
-  children,
-  opt,
-  type,
-  querySelector,
-  sectionTitle,
-  setLocation,
-  logo,
-  url,
+	children,
+	opt,
+	type,
+	querySelector,
+	sectionTitle,
+	setLocation,
+	logo,
+	url,
 }) => {
-  function renderComponent(renderThis) {
-    switch (renderThis) {
-      case "BODY":
-        return <BodyContainer children={children} opt={opt} />;
-      case "FOOTER":
-        return <FooterContainer />;
-      case "HEADER":
-        return <HeaderContainer heroData={opt} logo={logo} />;
-      case "ROW":
-        return (
-          <RowContainer
-            opt={opt}
-            children={children}
-            querySelector={querySelector}
-            sectionTitle={sectionTitle}
-            setLocation={setLocation}
-          />
-        );
-      case "FULLSCREEN":
-        return <FullScreenContainer opt={opt} children={children} />;
-      case "VIDEOPLAYER":
-        return <PlayerContainer opt={opt} children={children} url={url} />;
-      case "COMPONENT":
-        return <ComponentContainer opt={opt} />;
-      case "FORM":
-        return <FormContainer heroData={opt} />;
-      case "MAIN":
-        return <MainContainer opt={opt} />;
-      default:
-        return console.log(renderThis);
-    }
-  }
-  return <>{renderComponent(type)}</>;
-};
-export default LayoutResolver;
+	function renderComponent(renderThis) {
+		switch (renderThis) {
+			case 'BODY':
+				return <BodyContainer children={children} opt={opt} />
+			case 'FOOTER':
+				return <FooterContainer />
+			case 'HEADER':
+				return <HeaderContainer heroData={opt} logo={logo} />
+			case 'ROW':
+				return (
+					<RowContainer
+						opt={opt}
+						children={children}
+						querySelector={querySelector}
+						sectionTitle={sectionTitle}
+						setLocation={setLocation}
+					/>
+				)
+			case 'FULLSCREEN':
+				return <FullScreenContainer opt={opt} children={children} />
+			case 'VIDEOPLAYER':
+				return <PlayerContainer opt={opt} children={children} url={url} />
+			case 'COMPONENT':
+				return <ComponentContainer opt={opt} />
+			case 'FORM':
+				return <FormContainer heroData={opt} />
+			case 'MAIN':
+				return <MainContainer opt={opt} />
+			case 'MEMORYGAME':
+				return <MemoryGameContainer opt={opt} />
+			default:
+				return console.log(renderThis)
+		}
+	}
+	return <>{renderComponent(type)}</>
+}
+export default LayoutResolver
--- src/pages/my-own.js
+++ src/pages/my-own.js
@@ -1,216 +1,77 @@
 import React, { useEffect, useState, useRef } from 'react'
 import Layout from '@Layout'
-
-import Swal from 'sweetalert2'
-import withReactContent from 'sweetalert2-react-content'
-import Container from '@material-ui/core/Container'
-
-import {
-	Dialog,
-	DialogActions,
-	DialogContent,
-	DialogContentText,
-	Button,
-	DialogTitle,
-} from '@material-ui/core'
-import CardContainer from '../modules/layout/containers/CardContainer'
-import Card01a from '../../static/assets/images/glicemia-card-01-a.svg'
-import Card01b from '../../static/assets/images/glicemia-card-01-b.svg'
-import Card02a from '../../static/assets/images/glicemia-card-02-a.svg'
-import Card02b from '../../static/assets/images/glicemia-card-02-b.svg'
-import Card03a from '../../static/assets/images/glicemia-card-03-a.svg'
-import Card03b from '../../static/assets/images/glicemia-card-03-b.svg'
-import Card04a from '../../static/assets/images/glicemia-card-04-a.svg'
-import Card04b from '../../static/assets/images/glicemia-card-04-b.svg'
-import Card05a from '../../static/assets/images/glicemia-card-05-a.svg'
-import Card05b from '../../static/assets/images/glicemia-card-05-b.svg'
-import Card06a from '../../static/assets/images/glicemia-card-06-a.svg'
-import Card06b from '../../static/assets/images/glicemia-card-06-b.svg'
-import Card07a from '../../static/assets/images/glicemia-card-07-a.svg'
-import Card07b from '../../static/assets/images/glicemia-card-07-b.svg'
-import Card08a from '../../static/assets/images/glicemia-card-08-a.svg'
-import Card08b from '../../static/assets/images/glicemia-card-08-b.svg'
+import Cardaa from '../../static/assets/images/glicemia-card-01-a.svg'
+import Cardab from '../../static/assets/images/glicemia-card-01-b.svg'
+import Cardba from '../../static/assets/images/glicemia-card-02-a.svg'
+import Cardbb from '../../static/assets/images/glicemia-card-02-b.svg'
+import Cardca from '../../static/assets/images/glicemia-card-03-a.svg'
+import Cardcb from '../../static/assets/images/glicemia-card-03-b.svg'
+import Cardda from '../../static/assets/images/glicemia-card-04-a.svg'
+import Carddb from '../../static/assets/images/glicemia-card-04-b.svg'
+import Cardea from '../../static/assets/images/glicemia-card-05-a.svg'
+import Cardeb from '../../static/assets/images/glicemia-card-05-b.svg'
+import Cardfa from '../../static/assets/images/glicemia-card-06-a.svg'
+import Cardfb from '../../static/assets/images/glicemia-card-06-b.svg'
+import Cardga from '../../static/assets/images/glicemia-card-07-a.svg'
+import Cardgb from '../../static/assets/images/glicemia-card-07-b.svg'
+import Cardha from '../../static/assets/images/glicemia-card-08-a.svg'
+import Cardhb from '../../static/assets/images/glicemia-card-08-b.svg'
 
 const uniqueElementsArray = [
 	{
 		type: 'Pikachu',
-		image: <Card01a />,
+		image: <Cardaa />,
 	},
 	{
 		type: 'ButterFree',
-		image: <Card02a />,
+		image: <Cardba />,
 	},
 	{
 		type: 'Charmander',
-		image: <Card03a />,
+		image: <Cardca />,
 	},
 	{
 		type: 'Squirtle',
-		image: <Card04a />,
+		image: <Cardda />,
 	},
 	{
 		type: 'Pidgetto',
-		image: <Card05a />,
+		image: <Cardea />,
 	},
 	{
 		type: 'Bulbasaur',
-		image: <Card06a />,
+		image: <Cardfa />,
 	},
 ]
 
 const secondElementsArray = [
 	{
 		type: 'Pikachu',
-		image: <Card01b />,
+		image: <Cardab />,
 	},
 	{
 		type: 'ButterFree',
-		image: <Card02b />,
+		image: <Cardbb />,
 	},
 	{
 		type: 'Charmander',
-		image: <Card03b />,
+		image: <Cardcb />,
 	},
 	{
 		type: 'Squirtle',
-		image: <Card04b />,
+		image: <Carddb />,
 	},
 	{
 		type: 'Pidgetto',
-		image: <Card05b />,
+		image: <Cardeb />,
 	},
 	{
 		type: 'Bulbasaur',
-		image: <Card06b />,
+		image: <Cardfb />,
 	},
 ]
 
-function shuffleCards(array) {
-	const length = array.length
-	for (let i = length; i > 0; i--) {
-		const randomIndex = Math.floor(Math.random() * i)
-		const currentIndex = i - 1
-		const temp = array[currentIndex]
-		array[currentIndex] = array[randomIndex]
-		array[randomIndex] = temp
-	}
-	return array
-}
-
-function DialogMigrate({
-	children,
-	disableBackdropClick,
-	disableEscapeKeyDown,
-	onClose,
-	...rest
-}) {
-	const handleClose = (event, reason) => {
-		if (disableBackdropClick && reason === 'backdropClick') {
-			return false
-		}
-
-		if (disableEscapeKeyDown && reason === 'escapeKeyDown') {
-			return false
-		}
-
-		if (typeof onClose === 'function') {
-			onClose()
-		}
-	}
-
-	return (
-		<Dialog onClose={handleClose} {...rest}>
-			{children}
-		</Dialog>
-	)
-}
-
 const IndexPage = ({ data, location }) => {
-	const [open, setOpen] = useState(true)
-
-	const [cards, setCards] = useState(
-		shuffleCards.bind(null, uniqueElementsArray.concat(secondElementsArray))
-	)
-	const [openCards, setOpenCards] = useState([])
-	const [clearedCards, setClearedCards] = useState({})
-	const [shouldDisableAllCards, setShouldDisableAllCards] = useState(false)
-	const [moves, setMoves] = useState(0)
-	const [showModal, setShowModal] = useState(false)
-	const [bestScore, setBestScore] = useState(
-		JSON.parse(localStorage.getItem('bestScore')) || Number.POSITIVE_INFINITY
-	)
-	const timeout = useRef(null)
-
-	const disable = () => {
-		setShouldDisableAllCards(true)
-	}
-	const enable = () => {
-		setShouldDisableAllCards(false)
-	}
-
-	const checkCompletion = () => {
-		if (Object.keys(clearedCards).length === uniqueElementsArray.length) {
-			setShowModal(true)
-			const highScore = Math.min(moves, bestScore)
-			setBestScore(highScore)
-			localStorage.setItem('bestScore', highScore)
-		}
-	}
-	const evaluate = () => {
-		const [first, second] = openCards
-		enable()
-		if (cards[first].type === cards[second].type) {
-			setClearedCards((prev) => ({ ...prev, [cards[first].type]: true }))
-			setOpenCards([])
-			return
-		}
-		// This is to flip the cards back after 500ms duration
-		timeout.current = setTimeout(() => {
-			setOpenCards([])
-		}, 500)
-	}
-	const handleCardClick = (index) => {
-		if (openCards.length === 1) {
-			setOpenCards((prev) => [...prev, index])
-			setMoves((moves) => moves + 1)
-			disable()
-		} else {
-			clearTimeout(timeout.current)
-			setOpenCards([index])
-		}
-	}
-
-	useEffect(() => {
-		let timeout = null
-		if (openCards.length === 2) {
-			timeout = setTimeout(evaluate, 300)
-		}
-		return () => {
-			clearTimeout(timeout)
-		}
-	}, [openCards])
-
-	useEffect(() => {
-		checkCompletion()
-	}, [clearedCards])
-	const checkIsFlipped = (index) => {
-		return openCards.includes(index)
-	}
-
-	const checkIsInactive = (card) => {
-		return Boolean(clearedCards[card.type])
-	}
-
-	const handleRestart = () => {
-		setClearedCards({})
-		setOpenCards([])
-		setShowModal(false)
-		setMoves(0)
-		setShouldDisableAllCards(false)
-		// set a shuffled deck of cards
-		setCards(shuffleCards(uniqueElementsArray.concat(uniqueElementsArray)))
-	}
-
 	return (
 		<Layout type="BODY" opt={{ titleSeo: `DMDD` }}>
 			<Layout
@@ -226,91 +87,14 @@ const IndexPage = ({ data, location }) => {
 				<Layout type="HEADER" /*logo={data.edu4Dev}*/ />
 				{/* <h1>{cardState}</h1> */}
 				{/* <h1>{counter}</h1> */}
-				<div className="App">
-					<header>
-						<h3>Play the Flip card game</h3>
-						<div>
-							Select two cards with same content consequtively to make them
-							vanish
-						</div>
-					</header>
-					<div className="container">
-						{cards.map((card, index) => {
-							return (
-								<CardContainer
-									key={index}
-									card={card}
-									index={index}
-									isDisabled={shouldDisableAllCards}
-									isInactive={checkIsInactive(card)}
-									isFlipped={checkIsFlipped(index)}
-									onClick={handleCardClick}
-								/>
-							)
-						})}
-					</div>
-					<footer>
-						<div className="score">
-							<div className="moves">
-								<span className="bold">Moves:</span> {moves}
-							</div>
-							{localStorage.getItem('bestScore') && (
-								<div className="high-score">
-									<span className="bold">Best Score:</span> {bestScore}
-								</div>
-							)}
-						</div>
-						<div className="restart">
-							<Button
-								onClick={handleRestart}
-								color="primary"
-								variant="contained"
-							>
-								Restart
-							</Button>
-						</div>
-					</footer>
-					<Container>
-						<DialogMigrate
-							open={showModal}
-							disableEscapeKeyDown
-							onClose={() => {
-								// Whatever you want to run here on close.
-								setOpen(false)
-							}}
-							aria-labelledby="alert-dialog-title"
-							aria-describedby="alert-dialog-description"
-						>
-							<DialogTitle id="alert-dialog-title">
-								Hurray!!! You completed the challenge
-							</DialogTitle>
-							<DialogContent>
-								<DialogContentText id="alert-dialog-description">
-									You completed the game in {moves} moves. Your best score is{' '}
-									{bestScore} moves.
-								</DialogContentText>
-							</DialogContent>
-							<DialogActions>
-								<Button onClick={handleRestart} color="primary">
-									Restart
-								</Button>
-							</DialogActions>
-						</DialogMigrate>
-					</Container>
+				<Layout
+					type="MEMORYGAME"
+					opt={{
+						uniqueElementsArray,
+						secondElementsArray,
+					}} /*logo={data.edu4Dev}*/
+				/>
 
-					{/* <Dialog
-						open={showModal}
-						disableEscapeKeyDown
-						onClose={() => {
-							// Whatever you want to run here on close.
-							setOpen(false)
-						}}
-						aria-labelledby="alert-dialog-title"
-						aria-describedby="alert-dialog-description"
-					>
-
-					</Dialog> */}
-				</div>
 				{/* <Layout type="MAIN" opt={'nothing here'} /> */}
 			</Layout>
 		</Layout>
