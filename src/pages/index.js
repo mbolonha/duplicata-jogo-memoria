2e63417c4df693da70caad6ce682578e702732ae
miltonbolonha <miltonbolonha@gmail.com>
2021-11-01 18:06:11 -0300

    bugfix/develop build error pt 5

--- src/pages/index.js
+++ src/pages/index.js
@@ -131,7 +131,7 @@ export default IndexPage
 
 export const queryIndex = graphql`
 	query imgsIndex {
-		logotipoIndex: file(relativePath: { eq: "jogodamemoria-logo.png" }) {
+		logotipoIndex: file(relativePath: { eq: "jogodamemoria-logo.jpg" }) {
 			childrenImageSharp {
 				gatsbyImageData(layout: FIXED, width: 224, quality: 100)
 			}
